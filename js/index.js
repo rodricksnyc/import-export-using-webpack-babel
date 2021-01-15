9807190920a32bcf01feda0b257851fc86e379c7
Elizabeth <elizabeth@Elizabeths-iMac.local>
2021-01-15 09:42:31 -0500

    EGRerggerge

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
-// $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Rockville&units=imperial&appid=b2f2b923be22181ef89baa544605b888", function(data) {
-//
-// console.log(data);
-//
-// var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-//
-// var temp = Math.floor(data.main.temp);
-//
-// var weather = data.weather[0].main;
-//
-//
-// $('.icon').attr('src', icon);
-// $(".temp").append(temp  + ' &#8457;');
-//
-// $(".weather").append(weather );
-// });
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Rockville&units=imperial&appid=b2f2b923be22181ef89baa544605b888", function(data) {
+
+console.log(data);
+
+var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+var temp = Math.floor(data.main.temp);
+
+var weather = data.weather[0].main;
+
+
+$('.icon').attr('src', icon);
+$(".temp").append(temp  + ' &#8457;');
+
+$(".weather").append(weather );
+});
 
 
 
